5d0b2b12fbd63704226e9b31119f3a5623c13f92
Nick Charlton <hello@nickcharlton.net>
2011-11-26 21:19:15 +0000

    Sets up a basic project.
    Sets the copyright/licensing correctly. Sets the flipside to explain
    what it is and what it's for.

--- DrawingPrimitives/AppDelegate.m
+++ DrawingPrimitives/AppDelegate.m
@@ -3,7 +3,7 @@
 //  DrawingPrimitives
 //
 //  Created by Nick Charlton on 26/11/2011.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Nick Charlton. Licensed under the MIT license.
 //
 
 #import "AppDelegate.h"
